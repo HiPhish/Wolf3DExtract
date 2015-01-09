c0c2c890b1c69ebddc8105c410bc4e31a7fd4053
HiPhish <hiphish@Aleksandars-iMac.local>
2015-01-09 11:34:00 +0100

    Converter for AdLib sound effect to WLF music
    The converter porgram is complete now and working. This concludes the
    last missing data format.

--- Source/snd2wlf.c
+++ Source/snd2wlf.c
@@ -4,12 +4,10 @@
  *  format files.
  *  
  *  The AdLib sound effects are stored in a format that's similar to, but not
- *  exactly the Wolfenstein 3D music format.
- *
- *  The original source of the program was written by Sune Mindall in Java and
- *  has been converted to C by HiPhish.
- *
- *  **NOTE:** This program is still work in progress.
+ *  exactly the Wolfenstein 3D music format. The audio data was meant to be
+ *  sent to the sound hardware in a very specific way, so only the variable
+ *  data was stored. This converter adds the omitted information back so the
+ *  sound can be played like a music track.
  *
  *  The format of WLF music files is a variation of the IMF (id music format)
  *  specification. The only difference is that the lengh of a tick in an IMF
@@ -19,18 +17,21 @@
  *
  *  | Type         | Name       | Description                    |
  *  |--------------|------------|--------------------------------|
- *  | Uint32le     | length     | Length of the sound data       |
+ *  | Uint32le     | length     | Length of the audio data       |
  *  | Uint16le     | priority   | Priority of the sound effect   |
  *  | Byte[16]     | instrument | Instrument settings            |
  *  | Byte         | octave     | Octave number                  |
  *  | Byte[length] | data       | Actual sound data              |
- *  | Uint8        | terminator | Unused?                        |
- *  | Char[]       | name       | null-terminated name, optional |
+ *  | Uint8        | terminator | Unused                         |
+ *  | Char[]       | name       | Null-terminated name, optional |
  *
  *  The length and priority are *header* data and irrelevant to the actual
  *  effect, they only serve for the interpreting program to know how to play
  *  back the sound effect. The terminator and the name likewise are a *footer*
- *  and only relevant to *MUSE*, the program they were created with.
+ *  and only relevant to *Muse*, the program they were created with.
+ *
+ *  The original source of the program was written by Sune Mindall in Java and
+ *  has been ported to C by HiPhish.
  */
 
 #include <stdint.h>
@@ -39,39 +40,63 @@
 
 /*-[ CONSTANTS ]--------------------------------------------------------------*/
 
-#define MIN_DELAY   0 ///< asdf
+#define MIN_DELAY   0 ///< The shortest delay that can be used.
 #define TICK_DELAY  5 ///< One AdLib-sound-tick(@140hz) is 5 WLF ticks(@700Hz)
 
-//offsets in AdLib-soundchunk
-#define DATASZ_OFFSET     0 ///< uint32_t ie length 4
-#define PRIORITY_OFFSET   4 ///< uint16_t ie length 2
-#define INSTR_OFFSET      6 ///< uint8_t[16] (uint 8 x 16) length 16
-#define OCT_OFFSET       22 ///< uint8_t length = 1
-#define DATA_OFFSET      23 ///< uint8_t[*] length specified in dataSz.
+/** List of AdLib registers.
+ *
+ *  The reason I don't just make the numeric value of the items the hardware
+ *  addresses right away is so I could be able to iterate over the sequence of
+ *  registers when reading and writing in one go.
+ */
+enum adlib_registers {
+	M_CHAR   ,///< Modulator characteristics.
+	C_CHAR   ,///< Carrier characteristics.
+	M_SCALE  ,///< Modulator scale.
+	C_SCALE  ,///< Carrier scale.
+	M_ATTACK ,///< Modulator attack/decay rate.
+	C_ATTACK ,///< Carrier attack/decay rate.
+	M_SUST   ,///< Modulator sustain.
+	C_SUST   ,///< Carrier sustain.
+	M_WAVE   ,///< Modulator waveform.
+	C_WAVE   ,///< Carrier waveform.
+	N_CONN   ,///< Feedback/connection (usually ignored and set to 0).
 
-//AdLib registers (all bytes)
-#define M_CHAR    ((uint8_t)0x20) ///< Modulator characteristics.
-#define C_CHAR    ((uint8_t)0x23) ///< Carrier characteristics.
-#define M_SCALE   ((uint8_t)0x40) ///< Modulator scale.
-#define C_SCALE   ((uint8_t)0x43) ///< Carrier scale.
-#define M_ATTACK  ((uint8_t)0x60) ///< Modulator attack/decay rate.
-#define C_ATTACK  ((uint8_t)0x63) ///< Carrier attack/decay rate.
-#define M_SUST    ((uint8_t)0x80) ///< Modulator sustain.
-#define C_SUST    ((uint8_t)0x83) ///< Carrier sustain.
-#define M_WAVE    ((uint8_t)0xE0) ///< Modulator waveform.
-#define C_WAVE    ((uint8_t)0xE3) ///< Carrier waveform.
-#define N_CONN    ((uint8_t)0xC0) ///< Feedback/connection (usually ignored and set to 0).
+	NUMBER_OF_REGISTERS
+};
+
+/*-[ MAPPINGS ]---------------------------------------------------------------*/
+
+/** Map an AdLib register to its hardware address */
+uint8_t register_adress[NUMBER_OF_REGISTERS] = {
+	[M_CHAR  ] = 0x20,
+	[C_CHAR  ] = 0x23,
+	[M_SCALE ] = 0x40,
+	[C_SCALE ] = 0x43,
+	[M_ATTACK] = 0x60,
+	[C_ATTACK] = 0x63,
+	[M_SUST  ] = 0x80,
+	[C_SUST  ] = 0x83,
+	[M_WAVE  ] = 0xE0,
+	[C_WAVE  ] = 0xE3,
+	[N_CONN  ] = 0xC0,
+};
 
 
 /*-[ FUNCTION DECLARATIONS ]--------------------------------------------------*/
 
+/** Convert an AdLib sound effect file from the standard input to a WLF music
+ *  file to the standard output.
+ */
 int convert_sound(void);
 
 /** Write an IMF data element to the standard output.
  *
- *  @param reg    The OPL register to write.
- *  @param data   Data to write to the register.
- *  @param delay  Delay before next element, specified in ticks.
+ *  @param reg      The OPL register to write.
+ *  @param data     Data to write to the register.
+ *  @param delay    Delay before next element, specified in ticks.
+ *  @param counter  Pointer to a counter integer to keep track up how many
+ *                  bytes we have written.
  * 
  *  An IMF data element consists of four bytes:
  *
@@ -81,13 +106,23 @@ int convert_sound(void);
  *
  *  A tick is either 1/560 sec for regular IMF or 1/700 sec for WLF.
  */
-void write_imf_element(uint8_t reg, uint8_t data, uint16_t delay);
+void write_imf_element(uint8_t opl, uint8_t data, uint16_t delay, uint16_t *counter);
+
+/** Handles the arguments supplied to the program.
+ *  
+ *  @param argc  Number of arguments passed.
+ *  @param argv  Array of argument literals passed.
+ */
+void handle_arguments(int argc, char *argv[]);
+
+/** Prints usage instructions to the standard output. */
+void print_usage(void);
 
 /** Reads an unsigned 8-bit integer from file in an endian-independent way.
  *
  *  @return  The integer that was read.
  * 
- *  Reading an integer advances the file position by the data type size of the integer.
+ *  Reading advances the file position pointer.
  */
 uint32_t read_uint8(void);
 
@@ -95,7 +130,7 @@ uint32_t read_uint8(void);
  *
  *  @return  The integer that was read.
  *
- *  Reading an integer advances the file position by the data type size of the integer.
+ *  Reading advances the file position pointer.
  */
 uint32_t read_uint16(void);
 
@@ -103,14 +138,20 @@ uint32_t read_uint16(void);
  *
  *  @return  The integer that was read.
  *
- *  Reading an integer advances the file position by the data type size of the integer.
+ *  Reading advances the file position pointer.
  */
 uint32_t read_uint32(void);
+
+/** Write an unsigned 16-bit integer to the standard output.
+ *
+ *  @param i  The integer to write.
+ */
+void write_uint16(uint16_t i);
 
 
 /*----------------------------------------------------------------------------*/
 int main(int argc, char *argv[]) {
-	return 0;
+	return convert_sound();
 }
 /*----------------------------------------------------------------------------*/
 
@@ -118,58 +159,103 @@ int main(int argc, char *argv[]) {
 /*-[ FUNCTION IMPLEMENTATIONS ]-----------------------------------------------*/
 
 int convert_sound() {
-	//Read input data size (from header)
-	uint32_t data_size = read_uint32();
+	//Read length of the sound data from the header
+	uint32_t length = read_uint32();
+	
+	// Write placeholder for the length of the output
+	write_uint16(0x0000);
+
+	// The real length of the WLF music data
+	uint16_t wlf_length = 0;
 	
 	//Read priority
-	uint16_t priority  = read_uint16();
+	uint16_t priority = read_uint16();
 	
 	// Read and write instrument data
-	uint8_t  intstrument[16];
-	for (int i = 0; i < 16; ++i) {
-		intstrument[i] = read_uint8();
+	// nConn must never be set or the sound will play wrong
+	for (int reg = 0; reg < NUMBER_OF_REGISTERS - 1; ++reg) {
+		uint8_t intstrument = read_uint8();
+		write_imf_element(register_adress[reg], intstrument, MIN_DELAY, &wlf_length);
 	}
+	
+	// The last six bytes of the sequence are either padding or unused, skip ahead
+	fseek(stdin, 6, SEEK_CUR);
 
-	// Read octave and calculate block value
-	uint8_t block = read_uint8() << 2;
+	// Read octave and compute block value from it
+	uint8_t block = (read_uint8() & 7) << 2;
 
 	// Read and write the pitch data
-	int note_on = 0, i = DATA_OFFSET;
-	while (i < DATA_OFFSET + data_size) {
+	int note_on = 0; //boolean variable
+	for (int i = 0; i < length;) {
 		uint8_t note_value = read_uint8();
 		++i;
-		int repeated = 1;
+		uint16_t repeated = 1;
 		//look ahead to see if the pitch is repeated. If so, the repetitions is counted and we skip ahead. 
-		while ((i+1) < DATA_OFFSET + data_size && read_uint8() == note_value) {
+		while (i < length && read_uint8() == note_value) {
 			++repeated;
 			++i;
 		}
 		// step back one byte (remember reading bytes moves forward through the file)
 		fseek(stdin, -1, SEEK_CUR);
 		if (note_value == 0x00) { // note not to be played
-			write_imf_element(0xB0, block, TICK_DELAY * repeated);
+			write_imf_element(0xB0, block, TICK_DELAY * repeated, &wlf_length);
 			note_on = 0;
 		}
 		else if (!note_on) { // note to be played and status noteOFF
-			write_imf_element(0xA0, note_value  , MIN_DELAY            );
-			write_imf_element(0xB0, block | 0x20, TICK_DELAY * repeated);
+			write_imf_element(0xA0, note_value  , MIN_DELAY            , &wlf_length);
+			write_imf_element(0xB0, block | 0x20, TICK_DELAY * repeated, &wlf_length);
 			note_on = 1;
 		} else { // note to be played and status noteON
-			write_imf_element(0xA0, note_value, TICK_DELAY * repeated);
+			write_imf_element(0xA0, note_value, TICK_DELAY * repeated, &wlf_length);
 			note_on = 1;
 		}
 	}
 	// Add final note off
-	write_imf_element(0x0B, block, MIN_DELAY);
+	write_imf_element(0xB0, block, MIN_DELAY, &wlf_length);
 
-	//Read name(from footer) and priority
+	// skip terminator (fseek has stepped one byte back, so we have to skip two bytes forward)
+	fseek(stdin, 2, SEEK_CUR);
 
-	//Write blank dataSize in imf-header (we don't know this yet.) This is a UInt16LE
-	uint16_t imf_length = 0x0000;
+	// The audio data has been written, the rest is tag data that will be ignored
+	// by audio players. As such the data is not strictly standardised
+	
+	// The purpose of this is unknown
+	fprintf(stdout, "%c%c", '\0', '\0');
+
+	//Read name(from footer) and write it down (the name is always 16 characters)
+	for (int i = 0; i < 15; ++i) {
+		char c = read_uint8();
+		fprintf(stdout, "%c", c);
+		// reading the null character, that's the end of the string so break out
+		if (c  == '\0') {
+			// pad the rest of the string with '\0' to 16 characters
+			for (int j = 0; j < 15 - i - 1; ++j) {
+				fprintf(stdout, "%c", '\0');
+			}
+			break;
+		}
+	}
+	fprintf(stdout, "%c", '\0'); // terminate the string
+
+	// write comment (64 chars) and cProg (6 chars)
+	for (int i = 0; i < 70; ++i) {
+		fprintf(stdout, "%c", '\0');
+	}
+
+	// finally, write the WLF audio data length
+	fseek(stdout, 0, SEEK_SET); // seek to the beginning of the file
+	write_uint16(wlf_length);
 
 	return 0;
 }
 
+
+void write_imf_element(uint8_t opl, uint8_t data, uint16_t delay, uint16_t *counter) {
+	fwrite(&opl , sizeof(uint8_t), 1, stdout);
+	fwrite(&data, sizeof(uint8_t), 1, stdout);
+	write_uint16(delay);
+	if (counter != NULL) {*counter += 4;}
+}
 
 // In order to be able to read multi-byte value in an endian-independen manner
 // we first read the individual bytes in their original order, then we OR them
@@ -195,4 +281,10 @@ uint32_t read_uint32() {
 	return result;
 }
 
+void write_uint16(uint16_t i) {
+	uint8_t le = i & 0x00FF;  // little end
+	uint8_t be = i >> 8;      // big end
+	fwrite(&le, sizeof(uint8_t), 1, stdout);
+	fwrite(&be, sizeof(uint8_t), 1, stdout);
+}
 
